38da8e5aea531aecf9a2ef677a48bb96d87acf7c
Søren Lorentzen <sorenlorentzen@outlook.com>
2021-03-12 12:31:07 +0100

    Added a unittest for RowGenerator

---
+++ test/CodeChallenge.Core.Tests/RowGeneratorTests.cs
@@ -0,0 +1,26 @@
+using CodeChallenge.Core.Generators;
+using CodeChallenge.Core.Implementations;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+using System.ComponentModel.Design;
+
+namespace CodeChallenge.Core.Tests
+{
+    [TestClass]
+    public class RowGeneratorTests
+    {
+        [TestMethod]
+        public void TestNumbersArrayWillBePopulatedWithCorrectColsCount()
+        {
+            var numberGenerator = Substitute.For<INumberGenerator>();
+            numberGenerator.GenerateNumber(Arg.Any<int>()).Returns(20);
+
+            var rowGenerator = new RowGenerator(numberGenerator);
+            var colsCount = 12;
+            
+            var row = rowGenerator.GenerateRow(colsCount);
+            Assert.AreEqual(colsCount, row.Numbers.Length);
+        }
+
+    }
+}
