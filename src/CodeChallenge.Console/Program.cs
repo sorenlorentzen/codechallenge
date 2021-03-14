21668d2643f782f2f9e52e77bd8e9b2388d5944c
Søren Lorentzen <sorenlorentzen@outlook.com>
2021-03-14 12:28:33 +0100

    Added code to handle input and print output

--- src/CodeChallenge.Console/Program.cs
+++ src/CodeChallenge.Console/Program.cs
@@ -4,9 +4,11 @@ using CodeChallenge.Core.Models;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
@@ -34,10 +36,44 @@ namespace CodeChallenge.ConsoleApp
 
             var serviceProvider = services.BuildServiceProvider();
 
+            var tasksCount = Environment.ProcessorCount;
+
+            var keepGoing = true;
+            while (keepGoing)
+            {
+                await DoWork(serviceProvider, tasksCount);
+
+                Console.WriteLine("Type new degree of parallelism (eg. '10') or 'new' to restart. Enter to exit.");
+                var input = Console.ReadLine();
+
+
+
+                keepGoing = false;
+
+                if (int.TryParse(input, out var number))
+                {
+                    tasksCount = number;
+                    keepGoing = true;
+                }
+                else if (input.Equals("new", StringComparison.InvariantCultureIgnoreCase))
+                {
+                    tasksCount = Environment.ProcessorCount;
+                    keepGoing = true;
+                }
+            }
+
+        }
+
+        async static Task DoWork(IServiceProvider serviceProvider, int tasksCount)
+        {
+
+            var config = serviceProvider.GetRequiredService<Configuration>();
+            Console.WriteLine($"Starting to gennerate Coupon with {config.RowsInCoupon} rows of {config.ColumnsInRow} numbers with a max number of {config.MaxNumber}.");
+
             var sw = Stopwatch.StartNew();
-            
+
             var couponTasks = new List<CouponTask>();
-            for (var i = 0; i < Environment.ProcessorCount; i++)
+            for (var i = 0; i < tasksCount; i++)
             {
                 var couponTask = serviceProvider.GetRequiredService<CouponTask>();
                 couponTasks.Add(couponTask);
@@ -52,7 +88,33 @@ namespace CodeChallenge.ConsoleApp
             var avgCoupon = sw.ElapsedMilliseconds / (decimal)totalAttempts;
             Console.WriteLine($"Success after {couponTasks.Sum(x => x.Attempts)} attempts in {sw.Elapsed.TotalSeconds} seconds.");
             Console.WriteLine($"Each coupon took on average {avgCoupon:N5} ms.");
-            Console.WriteLine(JsonSerializer.Serialize(validCoupon));
+
+            PrintCoupon(validCoupon, config.LuckyNumber);
+        }
+
+        private static void PrintCoupon(Coupon coupon, int luckyNumber)
+        {
+            var defaultColor = Console.ForegroundColor;
+            foreach (var row in coupon.Rows)
+            {
+                Console.Write("| ");
+                foreach (var number in row.Numbers)
+                {
+                    var numberString = number.ToString().PadLeft(2, ' ');
+                    if (number == luckyNumber)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                        Console.Write(numberString);
+                        Console.ForegroundColor = defaultColor;
+                    }
+                    else
+                    {
+                        Console.Write(numberString);
+                    }
+                    Console.Write(" | ");
+                }
+                Console.Write(Environment.NewLine);
+            }
 
         }
 
