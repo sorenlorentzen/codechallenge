6b9e7237dbfb305cadc78aa0be52d2615552bfa8
Søren Lorentzen <sorenlorentzen@outlook.com>
2021-03-14 12:09:58 +0100

    Added configuration defaults

--- src/CodeChallenge.Core/Models/Configuration.cs
+++ src/CodeChallenge.Core/Models/Configuration.cs
@@ -10,18 +10,18 @@ namespace CodeChallenge.Core.Models
     public class Configuration
     {
         [JsonPropertyName("rowsInCoupon")]
-        public int RowsInCoupon { get; set; }
+        public int RowsInCoupon { get; set; } = 2;
 
         [JsonPropertyName("columnsInRow")]
-        public int ColumnsInRow { get; set; }
+        public int ColumnsInRow { get; set; } = 2;
 
         [JsonPropertyName("luckyNumber")]
-        public int LuckyNumber { get; set; }
+        public int LuckyNumber { get; set; } = 1;
 
         [JsonPropertyName("maxNumber")]
-        public int MaxNumber { get; set; }
+        public int MaxNumber { get; set; } = 3;
 
         [JsonPropertyName("useRandomNext")]
-        public bool UseRandomNext { get; set; }
+        public bool UseRandomNext { get; set; } = false;
     }
 }
